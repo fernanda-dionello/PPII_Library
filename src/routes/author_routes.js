0f4d642bea2d28678fa936261deaf96b116718e7
Fernanda Dionello de Mello <fernandadionellodemello@MacBook-Pro-de-Fernanda.local>
2021-10-25 18:55:04 -0300

    add the routes

--- src/routes/author_routes.js
+++ src/routes/author_routes.js
@@ -2,4 +2,10 @@ const express = require('express');
 const router = express.Router();
 const author_controller = require('../controllers/author_controller');
 
+router.get('/', author_controller.listAuthors);
+router.get('/:id', author_controller.listAuthorsById);
+router.post('/', author_controller.postAuthors);
+router.put('/', author_controller.putAuthors);
+router.delete('/:id', author_controller.deleteAuthors);
+
 module.exports = router;
--- src/routes/book_routes.js
+++ src/routes/book_routes.js
@@ -2,4 +2,10 @@ const express = require('express');
 const router = express.Router();
 const book_controller = require('../controllers/book_controller');
 
+router.get('/', book_controller.listBooks);
+router.get('/:id', book_controller.listBooksById);
+router.post('/', book_controller.postBooks);
+router.put('/', book_controller.putBooks);
+router.delete('/:id', book_controller.deleteBooks);
+
 module.exports = router;
