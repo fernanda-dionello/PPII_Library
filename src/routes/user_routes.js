911bad10b6c4f1f9abbb53e53052da295e895dff
Fernanda Dionello de Mello <fernandadionellodemello@MacBook-Pro-de-Fernanda.local>
2021-10-25 19:59:27 -0300

    add users controller

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const user_controller = require('../controllers/user_controller');
 
-router.post('/login', user_controller.postLogin);
+router.post('/login', user_controller.login);
 
 module.exports = router;
