dd30d96fef0742288612515e68cb449071e33436
Fernanda Dionello de Mello <fernandadionellodemello@MacBook-Pro-de-Fernanda.local>
2021-10-25 19:49:30 -0300

    add clients controller

--- src/routes/client_routes.js
+++ src/routes/client_routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const client_controller = require('../controllers/client_controller');
 
-router.get('/', client_controller.listBooks);
-router.get('/:id', client_controller.listBooksById);
-router.post('/', client_controller.postBooks);
-router.put('/', client_controller.putBooks);
-router.delete('/:id', client_controller.deleteBooks);
+router.get('/', client_controller.listClients);
+router.get('/:id', client_controller.listClientsById);
+router.post('/', client_controller.createClients);
+router.put('/', client_controller.updateClients);
+router.delete('/:id', client_controller.removeClients);
 
 module.exports = router;
