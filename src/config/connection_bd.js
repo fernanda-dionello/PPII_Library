0bafb802e50d885e41679d68395527e7f1e40d7f
Fernanda Dionello de Mello <fernandadionellodemello@MacBook-Pro-de-Fernanda.local>
2021-11-11 19:31:10 -0300

    change the database to Postgres Heroku testing connection

--- src/config/connection_bd.js
+++ src/config/connection_bd.js
@@ -1,13 +1,47 @@
-// const { Client } = require('pg');
+const { Client } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-const dbDatabase = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+// const connection = {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD
+// };
 
-const dbURL = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}`;
+// const client = new Client(connection);
+// client.connect();
+// const dbHost = process.env.DB_HOST;
+// const dbPort = process.env.DB_PORT;
+// const dbDatabase = process.env.DB_NAME;
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
 
-module.exports = dbURL;
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+});
+
+client.connect();
+
+// const client = new Client({
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//     host: process.env.DB_HOST,
+//     ssl: true
+// }); 
+// client.connect();
+
+
+// const dbURL = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}`;
+// const client = new Client({
+//     connectionString:`postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}` 
+// });
+// client.connect();
+
+module.exports = client;
