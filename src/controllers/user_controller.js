53fed5242caa7054c68009e40b421a1b56ff7cf0
Fernanda Dionello de Mello <fernandadionellodemello@MacBook-Pro-de-Fernanda.local>
2021-11-11 07:34:06 -0300

    add handling error - user_controller

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,12 +1,20 @@
 const user_repository = require('../repository/user_repository');
 
 exports.login = (req, resp) => {
-    let user = req.body;
-    user_repository.postLogin(user, (err, res) => {
-        if(err){
-            resp.status(500).json({err: err.message});
-        } else {
-            resp.status(201).json({msg: 'User logged!', user: res.rows});
-        };
-    });
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(username && password){
+        user_repository.postLogin(username, password, (err, res) => {
+            if(err){
+                resp.status(500).json({err: err.message});
+            } else if(res.rowCount == 0){
+                resp.status(404).json({msg:"Username or password wrong."});
+            } else {
+                resp.status(201).json({msg: 'User logged!', user: res.rows});
+            };
+        });
+    } else {
+        resp.status(400).json({msg:"Invalid data entry"});
+    };
 };
--- src/repository/user_repository.js
+++ src/repository/user_repository.js
@@ -1,7 +1,7 @@
 const connection = require('../config/connection_bd');
 
-exports.postLogin = (login, callback) => {
-    const values = [login.username, login.password];
+exports.postLogin = (username, password, callback) => {
+    const values = [username, password];
     connection.query('SELECT * FROM users WHERE username = $1 AND password = sha1($2)', values, (err, res) => {
         callback(err,res);
     });
