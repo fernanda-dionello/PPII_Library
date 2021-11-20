4e5494f9ee3a9f1d919f2804ce1f0c44a60b7f82
fernanda-dionello <fernanda.dionello@gmail.com>
2021-11-20 19:38:36 -0300

    add aditional search routes for books

--- src/repository/book_repository.js
+++ src/repository/book_repository.js
@@ -23,6 +23,29 @@ exports.getBookById = (id, callback) => {
     });
 };
 
+exports.getBooksByAuthorId = (id, callback) => {
+    const values = [id];
+    connection.query(`SELECT books.id, books.isbn, books.name, authors.name as "author", publishing_companies.name as "publishing_company", books.year
+    FROM books, authors, publishing_companies
+    WHERE books.author = authors.id
+    AND books.author = $1
+    AND books.publishing_company = publishing_companies.id`, values, (err, res) => {
+        callback(err,res);
+    });
+};
+
+exports.getAvailableBooks = (callback) => {
+    connection.query(`SELECT DISTINCT(books.id), books.isbn, books.name, authors.name as "author", publishing_companies.name as "publishing_company", books.year
+    FROM books, authors, publishing_companies, rents
+    WHERE books.author = authors.id
+    AND  books.publishing_company = publishing_companies.id
+	AND books.id != ALL(
+	select book_id from rents where active = true) 
+    ORDER BY books.id`, (err, res) => {
+        callback(err,res);
+    });
+};
+
 exports.postBook = (isbn, bookName, authorId, publishingCompanyId, year, callback) => {
     const values = [isbn, bookName, authorId, publishingCompanyId, year];
     connection.query('INSERT INTO books(isbn, name, author, publishing_company, year) VALUES ($1, $2, $3, $4, $5) RETURNING *', values, (err, res) => {
